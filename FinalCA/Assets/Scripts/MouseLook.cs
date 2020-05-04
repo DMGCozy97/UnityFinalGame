79a615d7e3f2b342814b3293a3b8adb9d9a8774d
DMGCozy97 <46888708+DMGCozy97@users.noreply.github.com>
2020-05-04 16:45:17 +0100

    dd
    dd

---
+++ FinalCA/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+
+    public transform playerBody;
+
+    float xRotation = 0f;
+
+	
+	void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float mouseX = Input.Getxis("MouseX") * mouseSensitivity * time.deltaTime;
+        float mouseY = Input.Getxis("MouseY") * mouseSensitivity * time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transfrom.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ FinalCA/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerAttack : MonoBehaviour
+{
+    
+
+    public Weapon[] weapons;
+    int activeWeaponIndex = -1;
+    public  Weapon activeWeapon;
+
+    public int index = 2;
+  
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        SetActiveWeapon(0);
+    }
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SetActiveWeapon(0);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SetActiveWeapon(1);
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            SetActiveWeapon(2);
+        if (activeWeapon == null) return;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            activeWeapon.Fire(transform.position);
+        }
+
+    }
+   
+    private void SetActiveWeapon(int index)
+    {
+       if(index != activeWeaponIndex)
+        {
+            if(index >= 0 && index <= weapons.Length)
+            {
+                if (activeWeapon)
+                    Destroy(activeWeapon.gameObject);
+
+                activeWeapon = Instantiate(weapons[index], transform);
+                activeWeaponIndex = index;
+            }
+        }
+    }
+
+   
+
+}
+
