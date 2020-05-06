94364e0e8768da56e8581954c89cd65004415fae
DMGCozy97 <46888708+DMGCozy97@users.noreply.github.com>
2020-05-06 14:04:21 +0100

    commit

---
+++ FinalCA/Assets/Scripts/Pistol.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class Pistol : RayCastWeapons
+{
+    public override void Fire(Vector3 fireFromePosition)
+    {
+        base.Fire(fireFromePosition);
+
+        if (Physics.Raycast(fireFromePosition, transform.forward, out RaycastHit, Range))
+        {
+            HealthComponent hitHealth = RaycastHit.transform.GetComponent<HealthComponent>();
+            if (hitHealth != null)
+                ApplyDamage(hitHealth);
+        }
+    }
+}
+
---
+++ FinalCA/Assets/Scripts/RayCastWeapons.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastWeapons : Weapon
+{
+    protected RaycastHit RaycastHit;
+    public float Range;
+    public float DamagePerHit;
+
+    protected void ApplyDamage(HealthComponent healthComponent)
+    {
+        healthComponent.ApplyDamage(DamagePerHit);
+    }
+}
